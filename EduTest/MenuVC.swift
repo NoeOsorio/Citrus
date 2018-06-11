f0b515a3cf55d7c6f9a9d89e70c1cd795205bc38
Noe <osorionoe@gmail.com>
2018-06-11 13:07:57 -0500

    Color changed

--- EduTest/MenuVC.swift
+++ EduTest/MenuVC.swift
@@ -19,10 +19,14 @@ class MenuVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
          ["materia":"Ciencias","icono":"atombw.png"],
          ["materia":"Geografia","icono":"earth-globebw.png"],
          ["materia":"Ingles","icono":"blackboardbw.png"]]
-    var colores = [UIColor(red:0.98, green:0.97, blue:0.97, alpha:1.0),
+    /*var colores = [UIColor(red:0.98, green:0.97, blue:0.97, alpha:1.0),
                    UIColor(red:0.38, green:0.94, blue:0.72, alpha:1.0),
                    UIColor(red:0.81, green:0.99, blue:0.53, alpha:1.0),
                    UIColor(red:0.56, green:0.68, blue:1.00, alpha:1.0)]
+    */
+    var colores = [UIColor(red:1.00, green:1.00, blue:0.62, alpha:1.0),
+                   UIColor(red:0.75, green:0.92, blue:0.62, alpha:1.0),
+                   UIColor(red:0.47, green:0.74, blue:0.56, alpha:1.0)]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return iconos.count
@@ -35,13 +39,14 @@ class MenuVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             cell.icon.image = UIImage(named: img)
         }
         cell.title.text = iconos[indexPath.row]["materia"]
-        cell.backgroundColor = colores[indexPath.row % 4]
+        cell.backgroundColor = colores[indexPath.row % colores.count]
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        performSegue(withIdentifier: "next", sender: self)
+       // performSegue(withIdentifier: "lectura", sender: self)
+         performSegue(withIdentifier: "ejercicio", sender: self)
     }
     //Esto evita que la celda se ponga gris a la hora de tocarla
     func tableView(_ tableView: UITableView, didHighlightRowAt indexPath: IndexPath) {
